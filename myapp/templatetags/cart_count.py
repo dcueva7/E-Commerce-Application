b915878aaca69f442991cafb6af8982f23665895
Daniel Cueva <dcueva@usc.edu>
2023-03-13 17:34:32 -0700

    Created django template tag cart_count to call function within index.html that grabs number of items in cart.  Number of items in cart now displays with cart icon

---
+++ myapp/templatetags/cart_count.py
@@ -0,0 +1,15 @@
+from django import template
+from myapp.models import Order
+
+register = template.Library()
+
+@register.filter 
+def item_count(user):
+    if user.is_authenticated:
+        order_qs = Order.objects.filter(user=user, ordered=False)
+        if order_qs.exists():
+            order = order_qs[0]
+            return order.items.count()
+        
+    return 0
+
